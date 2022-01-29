6215d8b9beee3a2707b3ac7a7ed6b280b9e567af
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-01-29 15:36:40 -0300

    função que formata o preco e salva nas variaveis de valor unitário

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,13 @@
+let priceDishe = null;
+let priceDrink = null;
+let priceDessert = null;
+
 
 function selectItem(item, foodCategory) {
     deselectItem(foodCategory);
     item.classList.add('active');
+
+    formatAndSavePrice(item, foodCategory);
 }
 
 function deselectItem(foodCategory) {
@@ -10,5 +16,18 @@ function deselectItem(foodCategory) {
 
     if (selecionado !== null) {
         selecionado.classList.remove('active');
+    }
+}
+
+
+function formatAndSavePrice(item, foodCategory) {
+    let valor = item.querySelector('.price h4').innerHTML.slice(3);
+
+    if (foodCategory === 'dishes') {
+        priceDishe = parseFloat(valor.replace(',', '.'))
+    }else if(foodCategory === 'drinks'){
+        priceDrink = parseFloat(valor.replace(',', '.'))
+    }else if(foodCategory === 'desserts'){
+        priceDessert = parseFloat(valor.replace(',', '.'))
     }
 }
