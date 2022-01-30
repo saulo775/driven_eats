61ee215fefd8ac7e8e398b7a1c997edd27aeb34f
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-01-30 10:44:33 -0300

    Finalização dos requisitos bonus

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,11 @@
 let priceDishe = null;
 let priceDrink = null;
 let priceDessert = null;
+
+let dishe;
+let drink;
+let dessert;
+
 let cont = 0;
 
 function selectItem(item, foodCategory) {
@@ -8,7 +13,7 @@ function selectItem(item, foodCategory) {
     item.classList.add('active');
     cont ++;
     if (cont>=3) {
-        activeButton();
+        activeSendButton();
     }
     formatAndSavePrice(item, foodCategory);
 }
@@ -35,37 +40,41 @@ function formatAndSavePrice(item, foodCategory) {
     }
 }
 
-function activeButton() {
+function activeSendButton() {
     let selecionado = document.querySelector('.button_container');
     selecionado.classList.add('button_active');
 
-    selecionado.querySelector('button').innerHTML = "Fechar pedido";
-
-    activateConfimationModal();
-    
+    selecionado.querySelector('button').innerHTML = "Fechar pedido"; 
 }
 
-function activateConfimationModal() {
-    let modal = document.querySelector('.modal');
-    modal.classList.add('activeModal');
+function confirmOrdered(params) {
+    
 }
 
 function sendOrdered() {
     let ordered = document.querySelectorAll('.active');
     
-    let dishes = ordered[0].querySelector('h3').innerText;
-    let drinks = ordered[1].querySelector('h3').innerText;
-    let dessert = ordered[2].querySelector('h3').innerText;
-    
+    dishe = ordered[0].querySelector('h3').innerText;
+    drink = ordered[1].querySelector('h3').innerText;
+    dessert = ordered[2].querySelector('h3').innerText;
+
     let total = (priceDessert + priceDishe + priceDrink);    
-    let numeroCelular = 559999999999;
+    let numeroCelular = 5594991791695;
+    let userName = window.prompt('Qual o seu nome:');
+    let userEnder = window.prompt('Qual o endereço de entrega (Rua/bairro/numero)');
 
     let mensagem=  `
     Olá, gostaria de fazer o pedido:
-- Prato: ${dishes}
-- Bebida: ${drinks}
+- Prato: ${dishe}
+- Bebida: ${drink}
 - Sobremesa: ${dessert}
-Total: R$ ${total}`;
+Total: R$ ${total}
+
+
+Nome: ${userName}
+Endereço: ${userEnder}
+`;
+
 
     let mensagemTransformada = encodeURIComponent(mensagem);
 
@@ -73,6 +82,37 @@ Total: R$ ${total}`;
     let mensagemWhatsapp = `https://wa.me/${numeroCelular}?text=${mensagemTransformada}`;
     
     window.open(mensagemWhatsapp);
+}
+
+function activeModal() {
+
+    if (cont >=3) {
+        let modal = document.querySelector(".modal");
+        modal.classList.add("activeModal");
+
+        let ordered = document.querySelectorAll('.active');
+    
+        dishe = ordered[0].querySelector('h3').innerText;
+        drink = ordered[1].querySelector('h3').innerText;
+        dessert = ordered[2].querySelector('h3').innerText;
+
+        let strPriceDishe = priceDishe.toString().replace(".",",");
+        let strPriceDrink = priceDrink.toString().replace(".",",");
+        let strPriceDessert = priceDessert.toString().replace(".",",");
+
+        let finalprice = (priceDishe + priceDrink + priceDessert).toFixed(2).toString().replace(".",",");
+
+        modal.querySelector('.product1 h5').innerText = `${dishe}`;
+        modal.querySelector('.product2 h5').innerText = `${drink}`;
+        modal.querySelector('.product3 h5').innerText = `${dessert}`;
+
+        modal.querySelector('.product1 h6').innerText = `${strPriceDishe}`;
+        modal.querySelector('.product2 h6').innerText = `${strPriceDrink}`;
+        modal.querySelector('.product3 h6').innerText = `${strPriceDessert}`;
+
+    
+        modal.querySelector('.finalPrice').innerText = `${finalprice}`
+    }
 }
 
 function closeModal() {
@@ -83,15 +123,3 @@ function closeModal() {
     }
 
 }
-
-// Olá, gostaria de fazer o pedido:
-// - Prato: ${prato1}
-// - Bebida: ${prato2}
-// - Sobremesa: ${prato3}
-// Total: R$ ${total}
-                
-// Nome: ${nomeUsuario}
-// Endereço: ${enderecoUsuario}
-// [5:44 AM]
-// let mensagemWhatsapp = https://wa.me/${numeroCelular}?text=${mensagemTransformada};
-
